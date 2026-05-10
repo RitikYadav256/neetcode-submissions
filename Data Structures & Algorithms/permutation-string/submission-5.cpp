67896b3f3283f2a12bd332e94f8d02e12a3fdfb0
RitikYadav256 <118496635+RitikYadav256@users.noreply.github.com>
2026-05-10 18:37:06 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+          if(s1.length()>s2.length()) return false;
+        int k=s1.length();
+        sort(s1.begin(),s1.end());
+        for(int i=0;i<=s2.length()-k;i++){
+            string sub=s2.substr(i,k);
+            sort(sub.begin(),sub.end());
+            if(sub==s1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
