7ed33976958abd424678dac90531eb7508b3eaa3
RitikYadav256 <118496635+RitikYadav256@users.noreply.github.com>
2026-05-10 15:23:08 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int price=0;
+
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[i]<prices[j]){
+                    price=max(price,prices[j]-prices[i]);
+                }
+            }
+
+        }
+        return price;
+    }
+};
